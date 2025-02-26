e0e5e60c6f8c3072e12a73ab9f67aa2c4a385b5a
涂兵兵 <2653038230@qq.com>
2025-02-26 23:39:24 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { Canvas } from './src/canvas/Canvas'
+import { Rect } from './src/shapes/Rect'
+import { Polygon } from './src/shapes/Polygon'
+import { Line } from './src/shapes/Line'
+import { Dot } from './src/shapes/Dot'
+import { Arrow } from './src/shapes/Arrow'
+import { Ruler } from './src/shapes/Ruler'
+import { Text } from './src/shapes/Text'
+import { Mask } from './src/shapes/Mask'
+import { Image as CanvasImage } from './src/shapes/Image'
+import { CrosshairLine } from './src/shapes/CrosshairLine'
+import { DrawObject } from './src/shapes/DrawObject'
+import { Matrix } from './src/utils/matrix'
+import { Control } from './src/control/Controls'
+
+
+export {
+    Canvas,
+    DrawObject,
+    Rect,
+    Polygon,
+    Line,
+    Dot,
+    Arrow,
+    Ruler,
+    Text,
+    Mask,
+    CrosshairLine,
+    Matrix,
+    CanvasImage,
+    Control
+}
